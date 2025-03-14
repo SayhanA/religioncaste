82e7c1406905f982bbcae798d4c38576ba8b9306
Sayhan Ahmed <sayhan@ewnbd.com>
2025-03-14 20:39:29 +0600

    commt

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error', error: err.message });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
